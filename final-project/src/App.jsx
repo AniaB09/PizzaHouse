5301b83207b6fab98da9d646914aab7aeb71d065
AniaB09 <nnpabian@gmail.com>
2024-07-17 21:46:13 +0200

    add header

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,3 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from "./components/header/Header.jsx";
 
+function App() {
+    return (
+
+            <div>
+                <Header />
+            </div>
+
+    )
+}
+
+export default App;
--- final-project/src/components/header/Header.jsx
+++ final-project/src/components/header/Header.jsx
@@ -1,2 +1,32 @@
 import React from 'react';
 import './header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="/home"><img
+                    src='https://www.svgrepo.com/show/215585/pizza.svg'
+                    width='30'
+                    height='30'
+                    className='d-inline-block align-top'
+                    alt='PizzaHouse-logo' /> PizzaHouse
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/home">Strona główna</Nav.Link>
+                        <Nav.Link href="/about">O nas</Nav.Link>
+                        <Nav.Link href="/order">Menu/Zamów</Nav.Link>
+                        <Nav.Link href="/contact">Kontakt</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
